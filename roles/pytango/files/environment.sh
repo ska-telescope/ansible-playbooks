b09bade86d541589e11db65118722f901e76dabc
Matteo04052017 <matteodicarlo@gmail.com>
2019-02-22 11:55:49 +0100

    fix

--- roles/pytango/files/environment.sh
+++ roles/pytango/files/environment.sh
@@ -1,7 +1,1 @@
-export LC_ALL=C.UTF-8 \
-        LANG=C.UTF-8 \
-        PIPENV_TIMEOUT=900 \
-        PATH=/venv/bin:$PATH \
-        VIRTUAL_ENV=/venv \
-        PIPENV_VERBOSITY=-1 \
-        PIPENV_NOSPIN=1
+export LC_ALL=C.UTF-8  LANG=C.UTF-8 PIPENV_TIMEOUT=900 PATH=/venv/bin:$PATH VIRTUAL_ENV=/venv PIPENV_VERBOSITY=-1 PIPENV_NOSPIN=1
