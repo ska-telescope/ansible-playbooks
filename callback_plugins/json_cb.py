3218696d74b21e005e5e79ea986033a62ad9ad9e
Bruno Morgado <jb.morgado@gmail.com>
2019-06-25 18:17:18 +0100

    added json output callback to ansible

---
+++ callback_plugins/json_cb.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from ansible.plugins.callback import CallbackBase
+import json
+
+class CallbackModule(CallbackBase):
+
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'stdout'
+    CALLBACK_NAME = 'json_cb'
+
+    def __init__(self):
+        self.tasks = {}
+
+    def dump_result(self, result):
+        print(json.dumps(dict(name=self.tasks[result._task._uuid],result=result._result)))
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self.tasks[task._uuid] = task.name
+
+    v2_runner_on_ok = dump_result
+    v2_runner_on_failed = dump_result
