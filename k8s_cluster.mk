b40744e4cb9628e5a1af58423492e3464587b750
Adriaan de Beer <adriaan@spaceadvisory.com>
2020-01-16 15:37:31 +0200

    fixed make help

--- k8s_cluster.mk
+++ k8s_cluster.mk
@@ -4,7 +4,7 @@ V_IPs ?= "192.168.100.25 192.168.100.26" ## Master node first, worker node after
 V_REMOTE_USER ?= ubuntu  ## remote username to access the machines nodes of k8s
 V_PRIVATE_SSH_KEY ?= ~/cloud.key  ## ssh private key to access the machines nodes of k8s
 
-V_KUBESPRAY_DIR ?=~/tmp/kubespray ## temporary directory for cloning and configuring kubespray
+V_KUBESPRAY_DIR ?= ~/tmp/kubespray ## temporary directory for cloning and configuring kubespray
 
 kubespray: ## create a k8s cluster using an existing inventory
 	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b -i $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME)/hosts.yaml $(V_KUBESPRAY_DIR)/cluster.yml --private-key=$(V_PRIVATE_SSH_KEY) && \
