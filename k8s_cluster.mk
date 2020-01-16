0a50137f1e6fa49bd6c6508905bda90b14ee822d
matteo1981 <matteo.dicarlo@inaf.it>
2020-01-16 17:19:40 +0000

    st-287 add master ip as variable

--- k8s_cluster.mk
+++ k8s_cluster.mk
@@ -1,5 +1,6 @@
 V_CLUSTERNAME ?= mycluster## Name of the cluster to be created
-V_IPs ?= "192.168.100.25 192.168.100.26" ## Master node first, worker node after
+V_IPs ?= "192.168.100.18 192.168.100.26" ## All internal IPs of the cluster
+V_MASTER_IP ?= "192.168.93.19" ## Master node floating ip
 
 V_REMOTE_USER ?= ubuntu## remote username to access the machines nodes of k8s
 V_PRIVATE_SSH_KEY ?= ~/cloud.key## ssh private key to access the machines nodes of k8s
@@ -7,7 +8,7 @@ V_PRIVATE_SSH_KEY ?= ~/cloud.key## ssh private key to access the machines nodes 
 V_KUBESPRAY_DIR ?= /usr/src/kubespray## temporary directory for cloning and configuring kubespray
 
 inventory: ## create the inventory files input for kubespray
-	ansible-playbook -b --extra-vars='{"cluster_name": $(V_CLUSTERNAME), "ips": $(V_IPs)}' k8s_create_inventory.yml
+	ansible-playbook -b --extra-vars='{"cluster_name": $(V_CLUSTERNAME), "ips": $(V_IPs), "master_floating_ip": $(V_MASTER_IP)}' k8s_create_inventory.yml
 
 kubespray: ## create a k8s cluster using an existing inventory
 	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b -i $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME)/hosts.yaml $(V_KUBESPRAY_DIR)/cluster.yml --private-key=$(V_PRIVATE_SSH_KEY) && \
