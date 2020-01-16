48d63cdadb4b2e72db4b7f078bccd85b2d26516a
matteo1981 <matteo.dicarlo@inaf.it>
2020-01-16 16:07:19 +0000

    ST-287 add dex and traefik installation

--- k8s_cluster.mk
+++ k8s_cluster.mk
@@ -6,19 +6,20 @@ V_PRIVATE_SSH_KEY ?= ~/cloud.key## ssh private key to access the machines nodes 
 
 V_KUBESPRAY_DIR ?= ~/tmp/kubespray## temporary directory for cloning and configuring kubespray
 
+inventory: ## create the inventory files input for kubespray
+	ansible-playbook --extra-vars='{"cluster_name": $(V_CLUSTERNAME), "ips": $(V_IPs)}' k8s_create_inventory.yml
+
 kubespray: ## create a k8s cluster using an existing inventory
 	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b -i $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME)/hosts.yaml $(V_KUBESPRAY_DIR)/cluster.yml --private-key=$(V_PRIVATE_SSH_KEY) && \
 	cp -rf $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME) ./inventory/$(V_CLUSTERNAME)
 
-cluster:  ## create a k8s cluster with ansible with IPs
-	ansible-playbook --extra-vars='{"cluster_name": $(V_CLUSTERNAME), "ips": $(V_IPs), "kubespray_dir": $(V_KUBESPRAY_DIR)}' k8s_create_inventory.yml -vvv
-	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b -i $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME)/hosts.yaml $(V_KUBESPRAY_DIR)/cluster.yml --private-key=$(V_PRIVATE_SSH_KEY)
-	cp -rf $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME) ./inventory/$(V_CLUSTERNAME) 
-	## TODO: 
-	## 1)helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-	## 2) traefik with external ip
+cluster: inventory ## create a k8s cluster with ansible with IPs
+	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b -i $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME)/hosts.yaml $(V_KUBESPRAY_DIR)/cluster.yml --private-key=$(V_PRIVATE_SSH_KEY) && \
+		cp -rf $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME) ./inventory/$(V_CLUSTERNAME) 
 
-reset:   ## reset the k8s cluster with ansible with IPs
-	ansible-playbook --extra-vars='{"cluster_name": "$(V_CLUSTERNAME)", "ips": "$(V_IPs)"}' k8s_create_inventory.yml && \
+components: ## install dex and traefik into the cluster specified in the kubeconfig
+	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b k8s_deploy_components.yml --private-key=$(V_PRIVATE_SSH_KEY)
+
+reset: inventory ## reset the k8s cluster with ansible with IPs
 	ansible-playbook --flush-cache -u $(V_REMOTE_USER) -b -i $(V_KUBESPRAY_DIR)/inventory/$(V_CLUSTERNAME)/hosts.yaml $(V_KUBESPRAY_DIR)/reset.yml --private-key=$(V_PRIVATE_SSH_KEY)
 
