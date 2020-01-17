5444f1dec54ee3f64f74815b588ed71b35d912e9
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2020-01-17 16:18:18 +0000

    ST-287 fix

--- k8s_cluster.mk
+++ k8s_cluster.mk
@@ -1,5 +1,5 @@
 V_CLUSTERNAME ?= mycluster## Name of the cluster to be created
-V_IPs ?= "192.168.100.18 192.168.100.26" ## All internal IPs of the cluster
+V_IPs ?= "192.168.100.45 192.168.100.15" ## All internal IPs of the cluster
 V_MASTER_IP ?= "192.168.93.19" ## Master node floating ip
 
 V_REMOTE_USER ?= ubuntu## remote username to access the machines nodes of k8s
@@ -8,10 +8,10 @@ V_PRIVATE_SSH_KEY ?= ~/cloud.key## ssh private key to access the machines nodes 
 V_KUBESPRAY_DIR ?= /usr/src/kubespray## temporary directory for cloning and configuring kubespray
 
 V_dex_issuer ?= "http://192.168.93.19:32000"
-V_dex_redirectURI ?= "http://192.168.93.13:32000/callback"
+V_dex_redirectURI ?= "http://192.168.93.19:32000/callback"
 V_gitlab_client_id ?= "417ea12283741e0d74b22778d2dd3f5d0dcee78828c6e9a8fd5e8589025b8d2f"
 V_gitlab_client_secret ?= "27a5830ca37bd1956b2a38d747a04ae9414f9f411af300493600acc7ebe6107f"
-V_dex_k8s_authenticator_redirectURI ?= "http://192.168.93.13:31000/callback/mycluster"
+V_dex_k8s_authenticator_redirectURI ?= "http://192.168.93.19:31000/callback/mycluster"
 
 V_k8s_master_uri ?= "https://192.168.93.19:6443"
 V_master_cert ?= "XXXXX"
